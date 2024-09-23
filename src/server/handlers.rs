9d9529547442ad3956d63a0adcb62de80c6170a1
Hexiang <qihexiang@outlook.com>
2024-09-23 16:49:43 +0800

    create layer and stack handlers

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{BTreeMap, BTreeSet},
+    ops::Range,
     sync::Arc,
 };
 
@@ -10,7 +11,9 @@ use axum::{
     Extension, Json,
 };
 use lme::{
-    chemistry::MoleculeLayer, layer::SelectOne, workspace::{LayerStorage, LayerStorageError, StackCache}
+    chemistry::MoleculeLayer,
+    layer::{Layer, SelectOne},
+    workspace::{LayerStorage, LayerStorageError, StackCache},
 };
 
 use serde::{Deserialize, Serialize};
@@ -22,14 +25,18 @@ pub async fn create_workspace(
     State(state): State<AppState>,
     Json(workspace): Json<WorkspaceName>,
 ) -> Response {
-    let workspaces = state.read().await;
-    if workspaces.contains_key(&workspace.name) {
+    let name_confilct = state.read().await.contains_key(&workspace.name);
+    if name_confilct {
         (
             StatusCode::CONFLICT,
             "Unable to create workspace, name already used.",
         )
             .into_response()
     } else {
+        state
+            .write()
+            .await
+            .insert(workspace.name, Default::default());
         StatusCode::OK.into_response()
     }
 }
@@ -74,30 +81,62 @@ impl From<LayerStorageError> for WorkspaceError {
 }
 
 pub async fn read_stack(
-    Extension(layers): Extension<Arc<RwLock<LayerStorage>>>,
+    Extension(layers_storage): Extension<Arc<RwLock<LayerStorage>>>,
     Extension(stacks): Extension<Arc<RwLock<BTreeMap<String, Vec<usize>>>>>,
     Extension(stack_cache): Extension<Arc<RwLock<StackCache>>>,
     Path(stack): Path<StackName>,
 ) -> Result<Json<MoleculeLayer>, Json<WorkspaceError>> {
-    let stacks = stacks
-        .read()
-        .await;
+    let stacks = stacks.read().await;
     let stack_path = stacks
         .get(&stack.stack_name)
-        .ok_or(Json(WorkspaceError::NoSuchStack(stack.stack_name.to_string())))?;
+        .ok_or(Json(WorkspaceError::NoSuchStack(
+            stack.stack_name.to_string(),
+        )))?;
     let cache = stack_cache.read().await.read_cache(&stack_path).cloned();
     if let Some(cached) = cache {
         Ok(Json(cached))
     } else {
-        let data = layers
+        let data = layers_storage
             .read()
             .await
-            .read_stack(&stack_path, Default::default()).map_err(|err| Json(WorkspaceError::from(err)))?;
+            .read_stack(&stack_path, Default::default())
+            .map_err(|err| Json(WorkspaceError::from(err)))?;
         stack_cache
             .write()
             .await
             .write_cache(&stack_path, data.clone());
         Ok(Json(data))
+    }
+}
+
+pub async fn create_layers(
+    Extension(layers_storage): Extension<Arc<RwLock<LayerStorage>>>,
+    Json(layers): Json<Vec<Layer>>,
+) -> Json<Range<usize>> {
+    Json(
+        layers_storage
+            .write()
+            .await
+            .create_layers(layers.into_iter()),
+    )
+}
+
+#[derive(Deserialize)]
+pub struct CreateStack {
+    name: String,
+    path: Vec<usize>,
+}
+
+pub async fn create_stack(
+    Extension(stacks): Extension<Arc<RwLock<BTreeMap<String, Vec<usize>>>>>,
+    Json(create_stack): Json<CreateStack>,
+) -> StatusCode {
+    let mut stacks = stacks.write().await;
+    if stacks.contains_key(&create_stack.name) {
+        StatusCode::CONFLICT
+    } else {
+        stacks.insert(create_stack.name, create_stack.path);
+        StatusCode::OK
     }
 }
 
@@ -109,9 +148,17 @@ async fn is_able_to_unlock() {
         .await
         .create_layers(vec![Default::default()].into_iter());
     let stacks: Arc<RwLock<BTreeMap<String, Vec<usize>>>> = Default::default();
-    // stacks.write().await.insert("example".to_string(), vec![1]);
+    stacks.write().await.insert("example".to_string(), vec![1]);
     let caches: Arc<RwLock<StackCache>> = Default::default();
     println!("Generate response");
-    let response = read_stack(Extension(layers), Extension(stacks), Extension(caches), Path(StackName { stack_name: "example".to_string() })).await;
+    let response = read_stack(
+        Extension(layers),
+        Extension(stacks),
+        Extension(caches),
+        Path(StackName {
+            stack_name: "example".to_string(),
+        }),
+    )
+    .await;
     println!("{:#?}", response.into_response());
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -3,8 +3,10 @@ use axum::{
     routing::{delete, get, post},
     Router,
 };
-use handlers::{create_workspace, get_layers, read_stack, remove_workspace};
-use lme::workspace::{StackCache, LayerStorage};
+use handlers::{
+    create_layers, create_stack, create_workspace, get_layers, read_stack, remove_workspace,
+};
+use lme::workspace::{LayerStorage, StackCache};
 use middlewares::workspace_middleware;
 use serde::{Deserialize, Serialize};
 use std::{collections::BTreeMap, sync::Arc};
@@ -13,7 +15,18 @@ use tokio::sync::RwLock;
 mod handlers;
 mod middlewares;
 
-pub type AppState = Arc<RwLock<BTreeMap<String, (Arc<RwLock<LayerStorage>>, Arc<RwLock<BTreeMap<String, Vec<usize>>>>, Arc<RwLock<StackCache>>)>>>;
+pub type AppState = Arc<
+    RwLock<
+        BTreeMap<
+            String,
+            (
+                Arc<RwLock<LayerStorage>>,
+                Arc<RwLock<BTreeMap<String, Vec<usize>>>>,
+                Arc<RwLock<StackCache>>,
+            ),
+        >,
+    >,
+>;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WorkspaceName {
@@ -24,16 +37,18 @@ pub struct WorkspaceName {
 async fn main() {
     let server_state: AppState = Default::default();
     let workspace_router = Router::new()
-    .route("/layers", get(get_layers))
-    .route("/stack", get(read_stack))
-    .layer(middleware::from_fn_with_state(
-        server_state.clone(),
-        workspace_middleware,
-    ));
+        .route("/stacks/new", post(create_stack))
+        .route("/layers/new", post(create_layers))
+        .route("/layers", get(get_layers))
+        .route("/stacks/:name", get(read_stack))
+        .layer(middleware::from_fn_with_state(
+            server_state.clone(),
+            workspace_middleware,
+        ));
     let app = Router::new()
         .nest("/workspace/:name", workspace_router)
-        .route("/create_workspace", post(create_workspace))
-        .route("/remove_workspace/:name", delete(remove_workspace))
+        .route("/workspace", post(create_workspace))
+        .route("/workspace/:name", delete(remove_workspace))
         .with_state(server_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
