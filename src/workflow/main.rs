2c1a70ff6639eb49ba83cd4fc4dd37b97c0b4cb2
Hexiang <qihexiang@outlook.com>
2024-09-27 23:38:22 +0800

    correct skip attribute in dump

--- src/workflow/main.rs
+++ src/workflow/main.rs
@@ -1,32 +1,41 @@
-use std::{fs::File, sync::{Arc, RwLock}};
+use std::{
+    fs::File,
+    sync::{Arc, RwLock},
+};
 
 use input_data::{WorkflowCheckPoint, WorkflowInput};
 use lme::workspace::StackCache;
 use workflow_data::WorkflowData;
 
 mod error;
-mod runner;
-mod workflow_data;
 mod input_data;
+mod runner;
 mod step;
+mod workflow_data;
 
 fn main() {
-    let input: WorkflowInput = serde_yaml::from_reader(File::open("lme_workflow.inp.yaml").unwrap()).unwrap();
-    let check: Option<WorkflowCheckPoint> = File::open("lme_workflow.chk.yaml").ok().and_then(|file| serde_yaml::from_reader(file).ok());
-    let (steps, mut workflow_data) = if let Some(check) = check {
-        let workflow_data = check.workflow_data;
-        let steps = input.steps.into_iter().skip(check.skip).collect();
+    let input: WorkflowInput =
+        serde_yaml::from_reader(File::open("lme_workflow.inp.yaml").unwrap()).unwrap();
+    let check_point: Option<WorkflowCheckPoint> = File::open("lme_workflow.chk.yaml")
+        .ok()
+        .and_then(|file| serde_yaml::from_reader(file).ok());
+    let (steps, mut workflow_data) = if let Some(check_point) = check_point {
+        let workflow_data = check_point.workflow_data;
+        let steps = input.steps.into_iter().skip(check_point.skip).collect();
+        println!("Workflow data loaded from checkpoint file.");
         (steps, workflow_data)
     } else {
         let workflow_data = WorkflowData::new(input.base);
         let steps = input.steps;
+        println!("Workflow data created from input file.");
         (steps, workflow_data)
     };
     let cache: Arc<RwLock<StackCache>> = Default::default();
     for (index, step) in steps.into_iter().enumerate() {
         step.execute(&mut workflow_data, cache.clone()).unwrap();
         let checkpoint = WorkflowCheckPoint {
-            skip: index, workflow_data: workflow_data.clone()
+            skip: index + 1,
+            workflow_data: workflow_data.clone(),
         };
         let file = File::create("lme_workflow.chk.yaml").unwrap();
         serde_yaml::to_writer(file, &checkpoint).unwrap();
