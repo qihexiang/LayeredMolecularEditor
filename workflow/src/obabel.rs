da39f8511ae0354226a547dc9a008afd482ada23
Hexiang <qihexiang@outlook.com>
2024-11-21 23:18:11 +0800

    mispell

--- workflow/src/obabel.rs
+++ workflow/src/obabel.rs
@@ -3,10 +3,11 @@ use std::{io::Write, process::{Command, Stdio}};
 use anyhow::{Ok, Result};
 
 pub fn obabel(input: &str, input_format: &str, output_format: &str) -> Result<String> {
-    let mut command = Command::new("sed")
+    let mut command = Command::new("obabel")
         .args([format!("-i{}", input_format), format!("-o{}", output_format)])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
+        .stderr(Stdio::null())
         .spawn()?;
     command.stdin.take().unwrap().write_all(input.as_bytes())?;
     let output = command.wait_with_output()?;
