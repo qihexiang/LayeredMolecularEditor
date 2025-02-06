60507d35bd0acb492ea04a2b336322db7cd69c61
Hexiang Qi <qihexiang@outlook.com>
2025-02-06 14:29:04 +0800

    fix bug of axis parallel to x axis

--- src/utils/geometric.rs
+++ src/utils/geometric.rs
@@ -3,7 +3,11 @@ use nalgebra::{Unit, Vector3};
 pub fn axis_angle_for_b2a(a: Vector3<f64>, b: Vector3<f64>) -> (Unit<Vector3<f64>>, f64) {
     let axis = b.cross(&a);
     let axis = Unit::new_normalize(if axis.norm() == 0. {
-        Vector3::x()
+        if a.cross(&Vector3::x()).norm() == 0. {
+            Vector3::y()
+        } else {
+            Vector3::x()
+        }
     } else {
         axis
     });
@@ -24,7 +28,7 @@ fn reverse_vectors() {
 fn same_direction() {
     println!(
         "{:#?}",
-        axis_angle_for_b2a(Vector3::new(1., 0., 0.), Vector3::new(4.5, 1e-8, 1e-16))
+        axis_angle_for_b2a(Vector3::new(1., 0., 0.), Vector3::new(-4.5, 1e-8, 1e-16))
     )
 }
 
